6c414ed32f09ae9f0f993efb3a87b7fde1f507b7
fengcaiwen <fengcaiwen@bytedance.com>
2024-01-01 16:44:07 +0800

    feat: update doc

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -2,68 +2,68 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-import Translate, {translate} from '@docusaurus/Translate';
+import Translate from '@docusaurus/Translate';
 
 type FeatureItem = {
-  title: JSX.Element;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+    title: React.JSX.JSXElement;
+    Svg: React.JSX.ComponentType<React.JSX.ComponentProps<'svg'>>;
+    description: React.JSX.JSXElement;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: <Translate>Remote to Local</Translate>,
-    Svg: require('@site/static/img/homepage_worldwide.svg').default,
-    description: (
-      <Translate>
-        access remote kubernetes cluster network
-      </Translate>
-    ),
-  },
-  {
-    title: <Translate>Local to Remote</Translate>,
-    Svg: require('@site/static/img/homepage_coding.svg').default,
-    description: (
-      <Translate>
-        remote kubernetes cluster service can also access your local service
-      </Translate>
-    ),
-  },
-  {
-    title: <Translate>And more</Translate>,
-    Svg: require('@site/static/img/homepage_team.svg').default,
-    description: (
-      <Translate>
-        run your kubernetes pod on local Docker container with same environment、volume、and network
-      </Translate>
-    ),
-  },
+    {
+        title: <Translate>Local to Remote</Translate>,
+        Svg: require('@site/static/img/homepage_coding.svg').default,
+        description: (
+            <Translate>
+                access remote kubernetes cluster network
+            </Translate>
+        ),
+    },
+    {
+        title: <Translate>Remote to Local</Translate>,
+        Svg: require('@site/static/img/homepage_worldwide.svg').default,
+        description: (
+            <Translate>
+                remote kubernetes cluster service can also access your local service
+            </Translate>
+        ),
+    },
+    {
+        title: <Translate>And more</Translate>,
+        Svg: require('@site/static/img/homepage_team.svg').default,
+        description: (
+            <Translate>
+                run your kubernetes pod on local Docker container with same environment、volume、and network
+            </Translate>
+        ),
+    },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img"/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
-export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+export default function HomepageFeatures(): React.JSX.JSXElement {
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -11,43 +11,44 @@ import styles from './index.module.css';
 import config from '@generated/docusaurus.config';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <img style={{ width: 300, height: 100, objectFit: "cover" }} src={require('@site/static/img/logo.jpeg').default} />
-        <p className="hero__subtitle">
-          <Translate>Cloud Native Dev Environment</Translate>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/quickstart">
-              <Translate>QuickStart</Translate>
-          </Link>
-          <Link
-            className={styles.homepageVersion}
-            to="https://github.com/KubeNetworks/kubevpn/releases/tag/v2.2.0">
-            v2.2.0
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <img style={{width: 300, height: 100, objectFit: "cover"}}
+                     src={require('@site/static/img/logo.jpeg').default}/>
+                <p className="hero__subtitle">
+                    <Translate>Cloud Native Dev Environment</Translate>
+                </p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/quickstart">
+                        <Translate>QuickStart</Translate>
+                    </Link>
+                    <Link
+                        className={styles.homepageVersion}
+                        to="https://github.com/KubeNetworks/kubevpn/releases/tag/v2.2.0">
+                        v2.2.0
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={translate({
-        message: "Homepage"
-      })}
-      description={config.tagline}>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+export default function Home(): React.JSX.Element {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={translate({
+                message: "Homepage"
+            })}
+            description={config.tagline}>
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+            </main>
+        </Layout>
+    );
 }
